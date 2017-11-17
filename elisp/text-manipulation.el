189e88cc8b73c0629488040963c9bac59ba01d3c
rushhike <rustamoff@gmail.com>
2017-11-17 17:33:35 +0500

    text-manipulation.el autoload

--- elisp/text-manipulation.el
+++ elisp/text-manipulation.el
@@ -1,6 +1,8 @@
-;;* 
+;;;###autoload
 (defun sort-symbols (reverse beg end)
       "Sort symbols in region alphabetically, in REVERSE if negative.
     See `sort-words'."
       (interactive "*P\nr")
       (sort-regexp-fields reverse "\\(\\sw\\|\\s_\\)+" "\\&" beg end))
+
+(provide 'text-manipulation)
