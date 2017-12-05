32e68207025645d3627eddf480d73e25bf0abdc9
rushhike <rustamoff@gmail.com>
2017-12-05 16:05:42 +0500

    rush-lua.el added

---
+++ modes/rush-lua.el
@@ -0,0 +1,10 @@
+(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
+(defun rush-lua-mode-company-init ()
+  (setq-local company-backends '((company-lua
+                                  company-etags
+                                  company-dabbrev-code
+                                  company-yasnippet))))
+(add-hook 'lua-mode-hook #'my-lua-mode-company-init)
